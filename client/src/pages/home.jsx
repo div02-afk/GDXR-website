401cf81c07b041539b0d1f64cb051725d3b49efc
Nepal Singh <nepalsss007@gmail.com>
2023-11-20 21:02:14 +0530

    new

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -4,21 +4,55 @@ export default function Home() {
   return (
     <div className="home">
       <div className="title">
-      Game Development <br/>and <br/> 
-      eXtended Reality club
+        Game Development <br />and <br />
+        eXtended Reality club
       </div>
       <div className="parallax-layer ship"></div>
       <div className="content">
         <div className="contentText">
-          AIT VR Tour
+
+          <div className="projectName">
+            <div className="eventImage">
+              <img src="images/vrimage.png" alt="vrimage" />
+            </div>
+            AIT VR Tour</div>
+          <div className="aboutEvnet">          Lorem, ipsum dolor sit amet consectetur lorem234 adipisicing </div>
+
+
         </div>
         <div className="contentText">
-          Genesis
+
+          <div className="projectName">
+            <div className="eventImage">
+              <img src="images/vrimage.png" alt="vrimage" />
+            </div>
+            AIT VR Tour</div>
+          <div className="aboutEvnet">          Lorem, ipsum dolor sit amet consectetur lorem234 adipisicing </div>
+
+
         </div>
         <div className="contentText">
-          Game-A-thon
+
+          <div className="projectName">
+            <div className="eventImage">
+              <img src="images/vrimage.png" alt="vrimage" />
+            </div>
+            AIT VR Tour</div>
+          <div className="aboutEvnet">          Lorem, ipsum dolor sit amet consectetur lorem234 adipisicing </div>
+
+
         </div>
-        <div className="contentText"></div>
+        <div className="contentText">
+
+          <div className="projectName">
+            <div className="eventImage">
+              <img src="images/vrimage.png" alt="vrimage" />
+            </div>
+            AIT VR Tour</div>
+          <div className="aboutEvnet">          Lorem, ipsum dolor sit amet consectetur lorem234 adipisicing </div>
+
+
+        </div>
       </div>
     </div>
   );
