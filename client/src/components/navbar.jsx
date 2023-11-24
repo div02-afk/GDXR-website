cc36eba9a31680c2bd68f8e1fa4dec6bdd3c320c
Gourav2609 <herculeswarrior.in@gmail.com>
2023-11-25 04:00:22 +0530

    Nav-bar update

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -3,20 +3,53 @@ import { useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHouse } from "@fortawesome/free-solid-svg-icons";
 import { faHeadset } from "@fortawesome/free-solid-svg-icons";
-import {faPeopleLine,faInfo} from "@fortawesome/free-solid-svg-icons";
+import { faPeopleLine, faInfo } from "@fortawesome/free-solid-svg-icons";
 export default function Navbar() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
   return (
     <div className="navBar">
-      <div className="logo" onClick = {()=>{window.location.href = "/"}}>
+      <div
+        className="logo"
+        onClick={() => {
+          window.location.href = "/";
+        }}
+      >
         <img src={logo} width={44} height={46} alt="Logo" />
       </div>
       <nav>
         <div className="navLinks">
-          <div className="navLink" onClick = {()=>{window.location.href = "/"}}><FontAwesomeIcon icon={faHouse} /></div>
-          <div className="navLink" onClick={()=>{window.location.href ="/#about"}}><FontAwesomeIcon icon={faInfo} style={{color: "#ffffff",}} /></div>
-          <div className="navLink" onClick={()=>{window.location.href ="/#team"}}><FontAwesomeIcon icon= {faPeopleLine}/></div>
-          <div className="navLink" onClick={()=>{window.location.href ="/#contact"}}><FontAwesomeIcon icon={faHeadset} /></div>
+          <div
+            className="navLink"
+            onClick={() => {
+              window.location.href = "/";
+            }}
+          >
+            <FontAwesomeIcon icon={faHouse} className="link-icon" />
+          </div>
+          <div
+            className="navLink"
+            onClick={() => {
+              window.location.href = "/#about";
+            }}
+          >
+            <FontAwesomeIcon icon={faInfo} style={{ color: "#ffffff" }} className="link-icon" />
+          </div>
+          <div
+            className="navLink"
+            onClick={() => {
+              window.location.href = "/#team";
+            }}
+          >
+            <FontAwesomeIcon icon={faPeopleLine} className="link-icon" />
+          </div>
+          <div
+            className="navLink"
+            onClick={() => {
+              window.location.href = "/#contact";
+            }}
+          >
+            <FontAwesomeIcon icon={faHeadset} className="link-icon" />
+          </div>
         </div>
       </nav>
     </div>
