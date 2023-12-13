3542fa485053b52ae8099faf0d8df252d5c1d7fe
Chetan Singh <chetan95497@gmail.com>
2023-12-13 18:54:12 +0530

    border and gradient for all the page box

--- client/src/pages/gameathon.jsx
+++ client/src/pages/gameathon.jsx
@@ -6,22 +6,26 @@ export default function Gameathon() {
     <div className="event w-full  text-white text-center xl:ml-32 xl:mr-4 lg:ml-24 lg:mr-5">
       <div className="event-title text-5xl mt-20 font-pixelEmulator">Game-A-thon</div>
       <div className="event-content flex flex-col lg:flex-row ">
-        <div className="eventContentText h-60 lg:w-2/5 w-3/4 flex flex-col justify-center m-auto mt-20 border-2 rounded-2xl bg-gray-900   backdrop-filter backdrop-blur-sm bg-opacity-10  border-gray-100">
-          <div className="projectName align-top ">
-            ABOUT
-          </div>
-          <div className="aboutEvent text-center ">
-            {" "}
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente dolorum sunt natus, animi officia asperiores quaerat deserunt, porro inventore a commodi minima, aperiam tempora autem sequi perspiciatis sint minus deleniti?{" "}
+        <div className="eventContentText h-60 lg:w-2/5 w-3/4 flex flex-col justify-center m-auto mt-20 rounded-2xl align-middle bg-gradient-to-br from-red-500 via-black to-blue-500 p-0.5">
+          <div className="h-full w-full bg-black rounded-2xl flex flex-col justify-center p-4">
+              <div className="projectName align-top ">
+              ABOUT
+            </div>
+            <div className="aboutEvent text-center ">
+              {" "}
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente dolorum sunt natus, animi officia asperiores quaerat deserunt, porro inventore a commodi minima, aperiam tempora autem sequi perspiciatis sint minus deleniti?{" "}
+            </div>
           </div>
         </div>
-        <div className="eventContentText h-60 lg:w-2/5 w-3/4 flex flex-col justify-center m-auto mt-20 border-2 rounded-2xl bg-gray-900   backdrop-filter backdrop-blur-sm bg-opacity-10  border-gray-100">
-          <div className="projectName ">
-            STATS
-          </div>
-          <div className="aboutEvent">
-            {" "}
-            Lorem, ipsum dolor sit amet consectetur lorem234 adipisicing{" "}
+        <div className="eventContentText h-60 lg:w-2/5 w-3/4 flex flex-col justify-center m-auto mt-20 rounded-2xl align-middle bg-gradient-to-br from-red-500 via-black to-blue-500 p-0.5">
+          <div className="h-full w-full bg-black rounded-2xl flex flex-col justify-center p-4">
+            <div className="projectName align-top">
+              STATS
+            </div>
+            <div className="aboutEvent">
+              {" "}
+              Lorem, ipsum dolor sit amet consectetur lorem234 adipisicing{" "}
+            </div>
           </div>
         </div>
       </div>
--- client/src/pages/genesis.jsx
+++ client/src/pages/genesis.jsx
@@ -3,29 +3,33 @@ import "./event.css";
 import { EmblaCarousel } from "../components/gallery";
 export default function Genesis() {
   return (
-    <div className="event w-full lg:h-1/2 text-white text-center xl:ml-32 xl:mr-4 lg:ml-24 lg:mr-5">
-      <div className="event-title text-5xl mt-20 font-pixelEmulator">Genesis</div>
+    <div className="event w-full  text-white text-center xl:ml-32 xl:mr-4 lg:ml-24 lg:mr-5">
+      <div className="event-title text-5xl mt-20 font-pixelEmulator">Game-A-thon</div>
       <div className="event-content flex flex-col lg:flex-row ">
-        <div className="eventContentText h-60 lg:w-2/5 w-3/4 flex flex-col justify-center m-auto mt-20 border-2 rounded-2xl bg-gray-900   backdrop-filter backdrop-blur-sm bg-opacity-10  border-gray-100">
-          <div className="projectName align-top ">
-            ABOUT
-          </div>
-          <div className="aboutEvent text-center ">
-            {" "}
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente dolorum sunt natus, animi officia asperiores quaerat deserunt, porro inventore a commodi minima, aperiam tempora autem sequi perspiciatis sint minus deleniti?{" "}
+        <div className="eventContentText h-60 lg:w-2/5 w-3/4 flex flex-col justify-center m-auto mt-20 rounded-2xl align-middle bg-gradient-to-br from-red-500 via-black to-blue-500 p-0.5">
+          <div className="h-full w-full bg-black rounded-2xl flex flex-col justify-center p-4">
+              <div className="projectName align-top ">
+              ABOUT
+            </div>
+            <div className="aboutEvent text-center ">
+              {" "}
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente dolorum sunt natus, animi officia asperiores quaerat deserunt, porro inventore a commodi minima, aperiam tempora autem sequi perspiciatis sint minus deleniti?{" "}
+            </div>
           </div>
         </div>
-        <div className="eventContentText h-60 lg:w-2/5 w-3/4 flex flex-col justify-center m-auto mt-20 border-2 rounded-2xl bg-gray-900   backdrop-filter backdrop-blur-sm bg-opacity-10  border-gray-100">
-          <div className="projectName ">
-            STATS
-          </div>
-          <div className="aboutEvent text-center">
-            {" "}
-            Lorem, ipsum dolor sit amet consectetur lorem234 adipisicing{" "}
+        <div className="eventContentText h-60 lg:w-2/5 w-3/4 flex flex-col justify-center m-auto mt-20 rounded-2xl align-middle bg-gradient-to-br from-red-500 via-black to-blue-500 p-0.5">
+          <div className="h-full w-full bg-black rounded-2xl flex flex-col justify-center p-4">
+            <div className="projectName align-top">
+              STATS
+            </div>
+            <div className="aboutEvent">
+              {" "}
+              Lorem, ipsum dolor sit amet consectetur lorem234 adipisicing{" "}
+            </div>
           </div>
         </div>
       </div>
-      <div className="gallery lg:mt-20">
+      <div className="gallery mt-20">
         <EmblaCarousel />
       </div>
     </div>
