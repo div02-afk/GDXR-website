77b22e3c1b651bcd0a44ab160e3b51860cf3a313
Divyam Sharma <113417008+div02-afk@users.noreply.github.com>
2023-12-15 11:05:27 +0530

    "navbar"

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -4,52 +4,43 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHouse } from "@fortawesome/free-solid-svg-icons";
 import { faHeadset } from "@fortawesome/free-solid-svg-icons";
 import { faPeopleLine, faInfo } from "@fortawesome/free-solid-svg-icons";
+import { motion } from "framer-motion";
 export default function Navbar() {
   const navigate = useNavigate();
+  const arr = [{link:"/#home",img:faHouse},{link:"/#about",img:faInfo},{link:"/#team",img:faPeopleLine},{link:"#contact",img:faHeadset}]
   return (
     <div className="navBar">
-      <div
+      <motion.div
         className="logo"
         onClick={() => {
           window.location.href = "/#home";
         }}
+        // whileHover={{
+        //   scale: 1.2,
+        //   transition: { duration: 0.3 },
+        // }}
+        // whileTap={{ scale: 0.9 }}
       >
+        
         <img src={logo} width={44} height={46} alt="Logo" />
-      </div>
+      </motion.div>
       <nav>
         <div className="navLinks">
-          <div
+          {arr.map(({link,img})=>(
+            <motion.div
+            whileHover={{
+              scale: 1.2,
+              transition: { duration: 0.3 },
+            }}
+            whileTap={{ scale: 0.9 }}
             className="navLink"
             onClick={() => {
-              window.location.href = "/#home";
+              window.location.href = link;
             }}
           >
-            <FontAwesomeIcon icon={faHouse} className="link-icon" />
-          </div>
-          <div
-            className="navLink"
-            onClick={() => {
-              window.location.href = "/#about";
-            }}
-          >
-            <FontAwesomeIcon icon={faInfo} style={{ color: "#ffffff" }} className="link-icon" />
-          </div>
-          <div
-            className="navLink"
-            onClick={() => {
-              window.location.href = "/#team";
-            }}
-          >
-            <FontAwesomeIcon icon={faPeopleLine} className="link-icon" />
-          </div>
-          <div
-            className="navLink"
-            onClick={() => {
-              window.location.href = "#contact";
-            }}
-          >
-            <FontAwesomeIcon icon={faHeadset} className="link-icon" />
-          </div>
+            <FontAwesomeIcon icon={img} className="link-icon" />
+          </motion.div>
+          ))}
         </div>
       </nav>
     </div>
