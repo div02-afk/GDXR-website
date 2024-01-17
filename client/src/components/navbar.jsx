82a03df9693050e114d7884d98cd62ff35b79865
Nepal Singh <nepalsss007@gmail.com>
2024-01-17 21:36:20 +0530

    Revert "something"
    This reverts commit 07a5e6aa6f9d019a3a242913a7902fae05e0155b.

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -1,11 +1,12 @@
 import logo from "../assets/Group 6 10.svg";
+import { useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHouse } from "@fortawesome/free-solid-svg-icons";
 import { faHeadset } from "@fortawesome/free-solid-svg-icons";
 import { faPeopleLine, faInfo } from "@fortawesome/free-solid-svg-icons";
 import { motion } from "framer-motion";
 export default function Navbar() {
-  
+  const navigate = useNavigate();
   const arr = [{link:"/#home",img:faHouse},{link:"/#about",img:faInfo},{link:"/#team",img:faPeopleLine},{link:"#contact",img:faHeadset}]
   return (
     <div className="navBar">
@@ -21,13 +22,12 @@ export default function Navbar() {
         // whileTap={{ scale: 0.9 }}
       >
         
-        <img src={logo} className="w-20" alt="Logo" />
+        <img src={logo} width={44} height={46} alt="Logo" />
       </motion.div>
       <nav>
         <div className="navLinks">
-          {arr.map(({link,img,index})=>(  
+          {arr.map(({link,img})=>(  
             <motion.div
-            key={index}
             whileHover={{
               scale: 1.2,
               transition: { duration: 0.3 },
@@ -38,7 +38,7 @@ export default function Navbar() {
               window.location.href = link;
             }}
           >
-            <FontAwesomeIcon icon={img} className="link-icon xl:text-3xl" />
+            <FontAwesomeIcon icon={img} className="link-icon" />
           </motion.div>
           ))}
         </div>
